9980f26fb0260a6f7e847d1dc1ad155c126e40ef
CarlxsMG <carlosmartiguerre@gmail.com>
2022-05-28 18:33:59 +0200

    Created payment model

--- Backend/server/apps/base/payment/models.py
+++ Backend/server/apps/base/payment/models.py
@@ -1,3 +1,30 @@
+# Django imports
 from django.db import models
 
+# Local imports
+from apps.base.contract.models import Contract
+
 # Create your models here.
+class Payment(models.Model):
+    """
+    Class to define customs Payments
+
+    Inheritances:
+        Model ([model]): Model class provided by django
+    """
+
+    _TYPES = [
+        ('S', 'Sell'),
+        ('F', 'Fee')
+    ]
+
+    _STATS = [
+        ('P', 'Pending'),
+        ('C', 'Completed')
+    ]
+
+    type = models.CharField(max_length=1, choices=_TYPES)
+    status = models.CharField(max_length=1, choices=_STATS)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    contract = models.ForeignKey(Contract, on_delete=models.DO_NOTHING)
