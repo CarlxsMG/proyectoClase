37d5f732c815f2c109511f9f531cb38486e77846
CarlxsMG <carlosmartiguerre@gmail.com>
2022-05-28 18:28:13 +0200

    Created contract model

--- Backend/server/apps/base/contract/models.py
+++ Backend/server/apps/base/contract/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from Backend.server.apps.base.user.models import BuyerUser, ManagementUser, SellerUser
+from Backend.server.apps.base.vehicle.models import Vehicle
+
 # Create your models here.
+class Contract(models.Model):
+    """
+    Class to define customs Vehicles
+
+    Inheritances:
+        Model ([model]): Model class provided by django
+    """
+
+    _STATS = [
+        ('P', 'Pending'),
+        ('C', 'Completed'),
+        ('R', 'Rejected')
+    ]
+
+    code = models.CharField(max_length=20)
+    status = models.CharField(max_length=1, choices=_STATS)
+    fee = models.IntegerField(max_length=2, default=2)
+
+    seller = models.ForeignKey(SellerUser, on_delete=models.DO_NOTHING)
+    buyer = models.ForeignKey(BuyerUser, on_delete=models.DO_NOTHING)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.DO_NOTHING)
+    manager = models.ForeignKey(ManagementUser, on_delete=models.DO_NOTHING)
