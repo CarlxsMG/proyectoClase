3966e793cf725d4900db0518d4947d01bb070ff9
CarlxsMG <carlosmartiguerre@gmail.com>
2022-05-28 21:34:09 +0200

    Added vehicle models to admin

--- Backend/server/apps/base/vehicle/admin.py
+++ Backend/server/apps/base/vehicle/admin.py
@@ -1,3 +1,16 @@
+# Django imports
 from django.contrib import admin
 
-# Register your models here.
+# Local imports
+from .models import Vehicle
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    
+    list_display = ('id', 'brand','model','status','price','plate','owner',)
+    list_display_links = ('id', 'owner',)
+    list_filter = ('status',)
+    search_fields = ['brand', 'model','price','plate','owner',]
+
+
+admin.site.register(Vehicle, VehicleAdmin)
