53826688fc835f9e12d91fe2fa43fd876cd378e8
CarlxsMG <carlosmartiguerre@gmail.com>
2022-05-28 21:30:31 +0200

    Added payment models to admin

--- Backend/server/apps/base/payment/admin.py
+++ Backend/server/apps/base/payment/admin.py
@@ -1,3 +1,16 @@
+# Django imports
 from django.contrib import admin
 
-# Register your models here.
+# Local imports
+from .models import Payment
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    
+    list_display = ('id', 'type', 'status','amount','contract',)
+    list_display_links = ('id', 'contract', )
+    list_filter = ('status', 'type',)
+    search_fields = ['amount',]
+
+
+admin.site.register(Payment, PaymentAdmin)
