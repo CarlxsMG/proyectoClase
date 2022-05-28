542847e9d1cba6e1b239a744ad021dea03bcd3c8
CarlxsMG <carlosmartiguerre@gmail.com>
2022-05-28 19:05:31 +0200

    Added viewsets, serializer and urls

---
+++ Backend/server/apps/base/user/serializers.py
@@ -0,0 +1,26 @@
+#  Third party imports
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, Serializer
+
+# Local imports 
+from .models import SellerUser, BuyerUser, ManagementUser
+
+
+# Serializers.
+class SellerSerializer(ModelSerializer):
+
+    class Meta:
+        model = SellerUser
+        exclude = ['password', 'is_superuser', 'is_staff']
+
+class BuyerSerializer(ModelSerializer):
+
+    class Meta:
+        model = BuyerUser
+        exclude = ['password', 'is_superuser', 'is_staff']
+
+class ManagementSerializer(ModelSerializer):
+
+    class Meta:
+        model = ManagementUser
+        exclude = ['password', 'is_superuser', 'is_staff']
