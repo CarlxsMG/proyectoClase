00c62d4d44fd07450619f08664a33c85577074a0
CarlxsMG <carlosmartiguerre@gmail.com>
2022-06-03 14:37:22 +0200

    First version of trades

--- Frontend/pages/seller/trade.vue
+++ Frontend/pages/seller/trade.vue
@@ -1,13 +1,42 @@
 <template>
-  
+  <main class="trade">
+      <hgroup class="trade-title">
+          <h1>Peticiones</h1>
+      </hgroup>
+      <p v-if="!trades.length" class="trade-no">
+          Aún no tienes ninguna petición :(
+      </p>
+      <ul class="trade-list">
+          <li class="trade-list-item" v-for="trade in trades" :key="trade">
+              <cards-trades />
+          </li>
+      </ul> 
+  </main>
 </template>
 
 <script>
 export default {
-
+    layout: 'seller',
+    data() {
+        return {
+            trades: [1,2,3,4,5]
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="sass" scoped>
+    .trade
+        padding: 2rem
+        max-width: 1200px
 
+        &-no
+            background: #e5d87f
+            text-align: center
+            font-weight: bold
+            padding: 1rem 2rem
+
+        &-list
+            display: grid
+            gap: 1rem
 </style>
