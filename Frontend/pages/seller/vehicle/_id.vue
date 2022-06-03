a35e52aaa829b755c0b0deae83ba0180b3a2aec0
CarlxsMG <carlosmartiguerre@gmail.com>
2022-06-03 13:38:02 +0200

    first fase of vehicles crreated and some extras in global styles

---
+++ Frontend/pages/seller/vehicle/_id.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="vehicle">
+      <hgroup class="vehicle-title">
+          <h1 v-if="newItem">Nuevo vehiculo</h1>
+          <h1 v-else>Editar vehiculo</h1>
+      </hgroup>
+      <p v-if="newItem" class="vehicle-guide">
+          Introduzca los campos correspondientes para publicar su vehiculo!
+      </p>
+      <p v-else class="vehicle-guide">
+          Edite los campos que prefiera!
+      </p>
+
+      <form-vehicle class="vehicle-form"/>
+  </main>
+</template>
+
+<script>
+export default {
+    layout: 'seller',
+    data() {
+        return {
+            newItem: this.$route.params.id
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+.vehicle    
+        padding: 2rem
+
+        &-guide
+            background: #e5d87f
+            text-align: center
+            font-weight: bold
+            padding: 1rem 2rem
+
+</style>
